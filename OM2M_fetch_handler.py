abfe6d3f82711db4b25ad31a65f9f2307a7c3b19
Divyansh Pandey <114301176+divyanash911@users.noreply.github.com>
2023-05-29 16:48:21 +0530

    Update OM2M_fetch_handler.py

--- OM2M_fetch_handler.py
+++ OM2M_fetch_handler.py
@@ -1,6 +1,6 @@
-import requests,jsonify,requests
-from flask import Flask
-url = "http://127.0.0.1:5089/~/in-cse/in-name/AE-TEST/Node-1/Data/la"
+import requests,requests
+from flask import Flask,jsonify
+url = "http://127.0.0.1:5089/~/in-cse/in-name/AE-TEST/User-Patterns/What_Room_Is_Grandma_In"
 from flask_cors import CORS
 app = Flask(__name__)
 
@@ -13,14 +13,25 @@ headers = {
   'Accept': 'application/json'
 }
 
+
+
 response = requests.request("GET", url, headers=headers, data=payload)
+print(response.text.replace(" ","").replace("\r\n","").split(":"))
+# print("-------------------")
 
-
-print(response.text)
 
 @app.route("/endpoint")
 def post_data():
-    return str(response.text)
+    response = requests.request("GET", url, headers=headers, data=payload)
+    for i in response.text.replace(" ","").replace("\r\n","").split(":"):
+      if 'lt' in i:
+          ind= response.text.replace(" ","").replace("\r\n","").split(":").index(i)
+          new= response.text.replace(" ","").replace("\r\n","").split(":")[ind+1]
+          new.split(",")
+          jsondict={"data":new[0]}
+          print(jsondict)
+    return jsonify(jsondict)
 
 if __name__ == '__main__':
     app.run(debug=True,port=8000)
+
