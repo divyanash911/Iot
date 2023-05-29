865aafebddae968cd9304611911bd9f8ea748268
AndyJocy <127510034+AndyJocy@users.noreply.github.com>
2023-05-29 17:26:03 +0530

    Update Flask

--- OM2M_fetch_handler.py
+++ OM2M_fetch_handler.py
@@ -1,6 +1,6 @@
-import requests,requests
+import requests,requests,json
 from flask import Flask,jsonify
-url = "http://127.0.0.1:5089/~/in-cse/in-name/User-Patterns/What_Room_Is_Grandma_In"
+url = "http://127.0.0.1:5089/~/in-cse/in-name/User-Patterns/What_Room_Is_Grandma_In/la"
 from flask_cors import CORS
 app = Flask(__name__)
 
@@ -16,22 +16,17 @@ headers = {
 
 
 response = requests.request("GET", url, headers=headers, data=payload)
-print(response.text.replace(" ","").replace("\r\n","").split(":"))
+print(json.loads(response.text))
 # print("-------------------")
 
 
 @app.route("/endpoint")
 def post_data():
     response = requests.request("GET", url, headers=headers, data=payload)
-    for i in response.text.replace(" ","").replace("\r\n","").split(":"):
-      if 'lt' in i:
-          ind= response.text.replace(" ","").replace("\r\n","").split(":").index(i)
-          new= response.text.replace(" ","").replace("\r\n","").split(":")[ind+1]
-          new.split(",")
-          jsondict={"data":new[0]}
-          print(jsondict)
+    dict=json.loads(response.text)
+    print(dict["m2m:cin"]["con"][1])
+    jsondict={"data":dict["m2m:cin"]["con"][1]}
     return jsonify(jsondict)
 
 if __name__ == '__main__':
     app.run(debug=True,port=8000)
-
